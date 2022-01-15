3ef11a3d6c6713be62cc84b7246db378540917b3
ShrimpandGits <44371907+ShrimpandGits@users.noreply.github.com>
2022-01-15 11:23:04 -0600

    Create Multi_calc.py

---
+++ Multi_calc.py
@@ -0,0 +1,9 @@
+
+
+#get user input for tree data 
+
+#split the tree data string into a list based on the "x" delimiter 
+treelist = trees.split('x')
+
+#just a simple test export 
+print (treelist)
